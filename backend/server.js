3ac7d7bfabf370e35e9c4dfbddf0d4904d4aaf9d
VInay Kumar <161389175+Deltavinay1@users.noreply.github.com>
2026-02-17 14:46:14 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend running 🚀");
+});
+
 // Multer config
 const upload = multer({ dest: "uploads/" });
 
