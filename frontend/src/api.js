4ec2b6e8a27a842147796d03b26340388611ecd0
VInay Kumar <161389175+Deltavinay1@users.noreply.github.com>
2026-02-17 14:25:46 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.BACKEND_URL,
 });
 
 export const interpretLogs = (wellId, curves, start, end) =>
